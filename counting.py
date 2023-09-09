9c51eb7369bebebbd218b0b789afc9d284eff7b3
Amit Raj <amitrajeknar@gmail.com>
2023-09-09 18:46:29 +0530

    count function added

--- counting.py
+++ counting.py
@@ -1,11 +1,5 @@
-print("The code have written below print the number from 1 to given number");
-
-n = int(input("Enter any integer no. : "))
-for i in range(1,n + 1) :
-
-      
-print("The End")
-
-    print(i)
-
-print("The End");
+def count(n):
+    for i in range(1,n+1):
+        print(i)
+        
+count(5)
